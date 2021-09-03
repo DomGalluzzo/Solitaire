63b74a4e24531fc31f335cb6a765a35d255e8fcc
Dominick Galluzzo <galluzdn@scps.k12.fl.us>
2021-09-03 14:10:08 -0400

    Initial commit with basic layout

---
+++ Solitaire/SolitaireGames/Cards.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solitaire.SolitaireGames
+{
+    public enum CardColor
+    {
+        Red, Black
+    }
+
+    public enum CardSuit
+    {
+        Hearts, Diamonds, Clubs, Spades
+    }
+
+    public enum CardType
+    {
+        // Hearts
+        HA, H2, H3, H4, H5, H6, H7, H8, H9, H10, HJ, HQ, HK,
+
+        // Diamonds
+        DA, D2, D3, D4, D5, D6, D7, D8, D9, D10, DJ, DQ, DK,
+
+        // Spades
+        SA, S2, S3, S4, S5, S6, S7, S8, S9, S10, SJ, SQ, SK,
+
+        // Clubs
+        CA, C2, C3, C4, C5, C6, C7, C8, C9, C10, CJ, CQ, CK
+    }
+    
+}
---
+++ Solitaire/SolitaireGames/PlayingCard.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Apex.MVVM;
+
+namespace Solitaire.SolitaireGames
+{
+    public class PlayingCard : ViewModel
+    {
+        public CardSuit Suit
+        {
+            get
+            {
+                CardType cardType = new();
+                int enumValue = (int)cardType;
+                if (enumValue < 13) { return CardSuit.Hearts; };
+
+                if (enumValue < 26) { return CardSuit.Diamonds; };
+
+                if (enumValue < 39) { return CardSuit.Clubs; };
+
+                return CardSuit.Spades;
+            }
+        }
+
+        //public int Value
+        //{
+        //    get
+        //    {
+
+        //    }
+        //}
+    }
+}
